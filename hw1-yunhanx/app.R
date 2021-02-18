4abc0cf0a183f77468f9395ff21b82101e309f12
Claireyhxu <70901824+Claireyhxu@users.noreply.github.com>
2021-02-18 23:17:33 +0800

    The second commit.

--- hw1-yunhanx/app.R
+++ hw1-yunhanx/app.R
@@ -6,43 +6,96 @@
 #
 #    http://shiny.rstudio.com/
 #
-
 library(shiny)
+library(ggplot2)
+library(DT)
+library(stringr)
+library(dplyr)
+library(tools)
+library(readr)
 
+police = read_csv("police.csv")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
+    # Theme selector --------------------------------------------------
+    shinythemes::themeSelector(),
+    theme = shinythemes::shinytheme("united"),
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Pittsburgh Police Arrests Data"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            # Select variable for y-axis ----------------------------------
+            selectInput(inputId = "y", 
+                      label = "Y-axis:",
+                      choices = c("Age" = "AGE", 
+                                  "Incident Zone" = "INCIDENTZONE", 
+                                  "Council District" = "COUNCIL_DISTRICT", 
+                                  "Public Works Division" = "PUBLIC_WORKS_DIVISION"), 
+                      selected = "INCIDENTZONE"),
+            
+            # Select variable for x-axis ----------------------------------
+            selectInput(inputId = "x", 
+                      label = "X-axis:",
+                      choices = c("Age" = "AGE", 
+                                  "Incident Zone" = "INCIDENTZONE", 
+                                  "Council District" = "COUNCIL_DISTRICT", 
+                                  "Public Works Division" = "PUBLIC_WORKS_DIVISION"), 
+                      selected = "AGE"),
+            
+            # Select variable for color -----------------------------------
+            selectInput(inputId = "z", 
+                      label = "Color by:",
+                      choices = c("Gender" = "GENDER", 
+                                  "Race" = "RACE"),
+                      selected = "RACE"),
+            # Set alpha level ---------------------------------------------
+            sliderInput(inputId = "alpha",
+                        label = "Alpha:",
+                        min = 0, max = 1,
+                        value = 0.5),
+            
+            # Set point size ----------------------------------------------
+            sliderInput(inputId = "size",
+                        label = "Size:",
+                        min = 0, max = 5,
+                        value = 2),
+            
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          # Show scatterplot --------------------------------------------
+          plotOutput(outputId = "scatterplot"),
+          br(),       
+
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  # Create a subset of data filtering for selected title types ------
+  police_subset <- reactive({
+    req(input$selected_type) # ensure availablity of value before proceeding
+    filter(police, title_type %in% input$selected_type)
+  })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  # Create scatterplot object the plotOutput function is expecting --
+  output$scatterplot <- renderPlot({
+    ggplot(data = police, aes_string(x = input$x, y = input$y,
+                                              color = input$z)) +
+      geom_point(alpha = input$alpha, size = input$size) +
+      labs(x = input$x,
+           y = input$y,
+           color = input$z,
+           title = ""
+      )
+  })
+
 }
 
 # Run the application 
